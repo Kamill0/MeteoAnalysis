9d68799b31aa15e4ba9d3e247affa2e705b26a23
Kamill0 <kamillo1493@gmail.com>
2017-09-20 00:35:03 +0200

    Some adjustments

--- Downloader.py
+++ Downloader.py
@@ -1,18 +1,50 @@
 import csv
 import urllib2
 import json
+import socket
+import os
+import datetime
+import copy
 
+class MyException(Exception):
+    pass
 
 class Downloader:
     BASEURL = 'http://mech.fis.agh.edu.pl/meteo/rest/'
 
-    def __init__(self, station="", typeOfMeasurement="", dateFrom="", dateTo="", dateStats=""):
+    station_coordinates = {
+        's000': ("50.0670", "19.9129"),
+        's001': ("50.0618", "19.9291"),
+        's002': ("50.057", "19.933"),
+        's003': ("49.8303", "19.9439"),
+        's004': ("50.275", "19.575"),
+        's005': ("50.13967", "19.39783"),
+        's006': ("49.7154", "20.4161"),
+        's007': ("50.0154", "20.9944"),
+        's008': ("49.9994", "20.928"),
+        's009': ("49.851", "19.344"),
+        's010': ("49.62161", "20.69555"),
+        's011': ("50.0447", "19.2111"),
+        's012': ("50.0464", "19.92219"),
+        's013': ("49.473253", "20.025243"),
+        's014': ("50.017", "20.9878"),
+        's015': ("50.0622", "19.9243"),
+        's016': ("49.848768", "20.794452"),
+        's017': ("49.9732", "19.8363"),
+        's018': ("49.6528", "21.1623"),
+        's501': ("50.01154", "19.9486")
+    }
+
+    def __init__(self, station="", typeOfMeasurement="", dateFrom="", dateTo="", dateStats="", monthly = False):
         self.station = station
         self.typeOfMeasurement = typeOfMeasurement
         self.dateFrom = dateFrom
+        self.start = dateFrom
         self.dateTo = dateTo
+        self.end = dateTo
         self.dateStats = dateStats
         self.cr = None
+        self.monthly = monthly
 
     # gets rid off the unicode sign in json
     def _byteify(self, data, ignore_dicts=False):
@@ -33,6 +65,16 @@ class Downloader:
             ignore_dicts=True
         )
 
+    def connection(self, webendpoint):
+        try:
+            response = urllib2.urlopen(webendpoint, timeout=10)
+            return response
+        except urllib2.URLError as e:
+            print(type(e))  # not catch
+        except socket.timeout as e:
+            print(type(e))  # catched
+            raise MyException("There was an error: %r" % e)
+
     def downloadData(self, webendpoint):
         response = urllib2.urlopen(webendpoint).read()
         if not response:
@@ -40,9 +82,31 @@ class Downloader:
             return
         self.cr = self.json_loads_byteified(response)
 
+    def last_day_of_month(self, any_day):
+        next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
+        return next_month - datetime.timedelta(days=next_month.day)
+
     def downloadAsCSVData(self, webendpoint):
-        response = urllib2.urlopen(webendpoint)
-        self.cr = csv.reader(response)
+        connection = self.connection(webendpoint)
+        self.cr = csv.reader(connection)
+        rows = []
+        for row in self.cr:
+            rows.append(row)
+
+        if len(rows) != 0:
+            filename = self.station + "_" + self.start + "_" + self.end
+            dirname = "Data/" + self.station
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            text_file = open(dirname + "/" + filename + ".csv", "wb")
+            writer = csv.writer(text_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for row in rows:
+                writer.writerow(row)
+
+            text_file.close()
+        else:
+            print("Response for: " + webendpoint + " was empty")
 
     def printData(self):
         for row in self.cr:
@@ -73,8 +137,25 @@ class Downloader:
 
     # historical data for specific station [CSV]
     def historicalCSV(self):
-        historicalCsvUrl = self.BASEURL + 'csv/' + self.typeOfMeasurement + '/' + self.station + '/' + self.dateFrom + '/' + self.dateTo
-        self.downloadAsCSVData(historicalCsvUrl)
+        if self.monthly:
+            dFrom = datetime.datetime.strptime(self.dateFrom, "%Y-%m-%d").date()
+            dTo = datetime.datetime.strptime(self.dateTo, "%Y-%m-%d").date()
+            dToLastDay = self.last_day_of_month(dFrom)
+            while dTo > dToLastDay:
+                self.start = str(dFrom)
+                self.end = str(dToLastDay)
+                historicalCsvUrl = self.BASEURL + 'csv/' + self.typeOfMeasurement + '/' + self.station + '/' + self.start + '/' + self.end
+                self.downloadAsCSVData(historicalCsvUrl)
+                dFrom = dToLastDay + datetime.timedelta(days=1)
+                dToLastDay = self.last_day_of_month(dFrom)
+            self.start = str(dFrom)
+            self.end = str(dTo)
+            historicalCsvUrl = self.BASEURL + 'csv/' + self.typeOfMeasurement + '/' + self.station + '/' + str(dFrom) + '/' + str(dTo)
+            self.downloadAsCSVData(historicalCsvUrl)
+        else:
+            historicalCsvUrl = self.BASEURL + 'csv/' + self.typeOfMeasurement + '/' + self.station + '/' + self.dateFrom + '/' + self.dateTo
+            self.downloadAsCSVData(historicalCsvUrl)
+
 
     # statistic for specific station: how many measures were registered [JSON]
     def statistic(self):
@@ -88,6 +169,17 @@ class Downloader:
 
 
 if __name__ == '__main__':
-    cs = Downloader('s000', 'temp', '2017-05-20', '2017-05-20')
-    print cs.getAllStations()
+    coordinates = Downloader.station_coordinates
+
+    for k in coordinates:
+        print(k)
+        cs = Downloader(k, 'temp', '2017-07-01', '2017-07-31')
+        cs.historicalCSV()
+
+
+
+
+
+
+
     #cs.printData()
--- first.R
+++ first.R
@@ -4,12 +4,12 @@ library(AnomalyDetection)
 #res = AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
 #res$plot
 
-dat = read.csv(file="C:\\Users\\kamil_000\\Documents\\2017-07-05.csv", header = FALSE)
+dat = read.csv(file="C:\\Users\\kamil_000\\PycharmProjects\\MeteoAnalysis\\Data\\2017-07-31.csv", header = FALSE)
 colnames(dat) <- c('time','time_meh','temp')
 dat <- dat[ -c(2)]
 dat$time <- as.POSIXct(paste(dat$time), format="%Y-%m-%d %H:%M:%S")
 dat <- dat[!duplicated(dat),]
-#dat$temp[1] = dat$temp[1]*2  #fake anomaly creation <- it works, yay 
+dat$temp[1] = dat$temp[1]*2  #fake anomaly creation <- it works, yay 
 res = AnomalyDetectionTs(dat, max_anoms=0.02, direction='both', plot=TRUE)
 if (is.null(res$plot)){
   print("No anomalies detected, nothing to plot here")
