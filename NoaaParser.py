696179ee74f2fcf0abcd9a686a902e69308db28e
Kamill0 <kamillo1493@gmail.com>
2017-09-22 20:28:51 +0200

    Added more measurements and separated the parsed file by them into subdirectories

--- NoaaParser.py
+++ NoaaParser.py
@@ -1,15 +1,24 @@
 from datetime import datetime
 import csv
+import os
 
 class NoaaParser:
+
+    # values are: (begining column, ending column, scaling factor)
+    type_of_measurements = {
+        'pres0': (99, 104, 10),
+        'temp': (87, 92, 10),
+        'temp0': (93, 98, 10),
+        'winds': (65, 69, 10)
+    }
+
     def __init__(self, path):
         self.path = path
 
-    def parseFile(self):
-        basePath = "DataNOAA/processed/"
+    def parseFile(self, measurement):
+        basePath = "DataNOAA/processed"
         textFiles = {}
         writers = {}
-        #writer = csv.writer(text_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
         with open("DataNOAA/source/" + self.path) as f:
             prev = ""
@@ -17,24 +26,33 @@ class NoaaParser:
                  dt = datetime.strptime(line[15:27], '%Y%m%d%H%M%S')
                  fileName = str(dt.year) + "-" + str(dt.month)
                  if fileName not in textFiles and fileName not in writers:
-                     textFiles[fileName] = open(basePath + "/temp_" + fileName + ".csv", "wb")
+                     dirName = basePath + "/" + measurement
+                     if not os.path.exists(dirName):
+                         os.makedirs(dirName)
+                     textFiles[fileName] = open(dirName + "/" + measurement + "_" + fileName + ".csv", "wb")
                      writers[fileName] = csv.writer(textFiles.get(fileName), delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                  writer = writers.get(fileName)
-                 airtemp_sign = line[87:88]
-                 airtemp_value = float(line[88:92])/10
-                 if airtemp_sign == "-":
-                     airtemp_value = airtemp_sign + str(airtemp_value)
+
+                 indexes = self.type_of_measurements.get(measurement)
+                 if measurement.startswith('temp'):
+                     temp_sign = line[indexes[0]:indexes[0]+1]
+                     value = float(line[indexes[0]+1:indexes[1]])/indexes[-1]
+                     if temp_sign == "-":
+                         value = temp_sign + str(value)
+                 else:
+                     value = float(line[indexes[0]:indexes[1]])/indexes[-1]
+
                  if dt.minute != 0:
                     dt = dt.replace(minute=0)
-                 if dt!=prev:
-                     #print(dt)
+                 if dt != prev and value * indexes[-1] < 9999:
                      prev = dt
-                     writer.writerow([dt, airtemp_value])
-            for k,v in textFiles.iteritems():
+                     writer.writerow([dt, value])
+            for k, v in textFiles.iteritems():
                 v.close()
 
 
 if __name__ == '__main__':
-    path = "125660-99999-2017"
+    path = "125660-99999-2016"
     parser = NoaaParser(path)
-    parser.parseFile()
+    for k in parser.type_of_measurements:
+        parser.parseFile(k)
